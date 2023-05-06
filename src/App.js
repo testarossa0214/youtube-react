7ec6820327d9d3cef4695efcbc199f374bf35cd2
testarossa0214 <soundlight@live.jp>
2023-05-06 16:38:56 +0900

    add api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Top from './pages/Top'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch" element={<Watch />} />
+      </Routes>
+    </Router>
   );
 }
 
